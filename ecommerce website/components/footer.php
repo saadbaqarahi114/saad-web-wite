4d7f3c59a01175c33bf6880ebbb27c907c0cbe27
saadbaqarahi114 <138329673+saadbaqarahi114@users.noreply.github.com>
2023-07-01 21:32:06 +0100

    Add files via upload

---
+++ ecommerce website/components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact us</h3>
+         <a href="tel:+212 645665567"><i class="fas fa-phone"></i> +212 645665567</a>
+         <a href="saadbaqarahi@gmail.com"><i class="fas fa-envelope"></i>saadbaqarahi@gmail.com</a>
+         <a href="https://www.google.com/maps/place/INSTITUT+DE+TECHNOLOGIE+APPLIQUEE+YACOUB+EL+MANSOUR+RABAT/@33.9697723,-6.8924088,16z/data=!4m10!1m2!2m1!1sISTA+YAACOUB+EL+MANSOUR!3m6!1s0xda76d2f98c0576d:0xa854b9d82563a76!8m2!3d33.9697723!4d-6.887259!15sChdJU1RBIFlBQUNPVUIgRUwgTUFOU09VUpIBB2NvbGxlZ2XgAQA!16s%2Fg%2F11c0vlv9j4?hl=fr-FR&entry=ttuhttps://www.google.com/"><i class="fas fa-map-marker-alt"></i>Maroc, Rabat - YM 54882 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Mr.Saad web developper</span> | all rights reserved!</div>
+
+</footer>
